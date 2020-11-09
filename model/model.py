85a3a97a12d6c0f0095034bae37412a25a858a72
KremerMichael <kremeremichael@gmail.com>
2020-11-09 10:19:52 -0500

    auto push run at Mon 09 Nov 2020 10:19:49 AM EST

--- model/model.py
+++ model/model.py
@@ -1,29 +1,31 @@
-##########################
-# This is our simmulation
-# may god help us lmao
-##########################
+#!/usr/bin/env python3
+############################
+### IMPORTS ###
 import datetime
 import time
+import csv
+import pandas
+import os
 
-################################
+####################################################################
 # GENERICS FOR SIMMULATION
-################################
-battery_charge = 0
-number_of_houses = 4
-house_running_cost = [0] * number_of_houses
-
-#both of these probably date time things
+####################################################################
+# set the starting date and time
 dt = datetime.datetime.now()
 dt = dt.replace(microsecond=0, second=0, minute=0, hour=0, day=1, month=1, year=2020)
 
 interval_length = 5 #(minutes)
 interval_count = 105120
+# given interval_length == 5
 #1 hour 60/interval_length = 12
 #1 day 1440/interval_length = 288
 #30 days 43200/interval_length = 8640
 #180 days 259200/interval_length = 51840
 #365 days 525600/interval_length = 105120
 
+####################################################################
+# CLASSES
+####################################################################
 class Battery_obj:
 
     def __init__(self):
@@ -47,9 +49,9 @@ class Battery_obj:
         self.curr_charge -= amount
         return amount * self.average_cost #return cost of this charge
 
-################################
-# Functions for simmulation
-################################
+####################################################################
+# FUNCTIONS 
+####################################################################
 # return the cost of power from the maingrid (dollars/kWh)
 # using this as refrence https://www.sce.com/residential/rates/Time-Of-Use-Residential-Rate-Plans
 # for now this is using the TOU-D-4-9PM plan, TODO: add other plans
@@ -85,25 +87,58 @@ def get_maingrid_cost():
         else: #winter, weekend and weekday, super off-peak
             return 0.25
 
-def house_data_call(date, time, interval_length, x):
-    # return energy used starting at date, time
-    # for length of time = interval_length
-    # in units kWh
-    # x is house num == year of data ie year1.txt, year2.txt
-    return 1
-
-
+####################################################################
+# MAIN
+####################################################################
+# set up battery object
 battery = Battery_obj()
 
-    
-################################
-# MAIN LOOP
-################################
+#set up timer
 timer_start = time.time()
+interval_count_historic = interval_count
+
+number_of_houses = 4 #do not change, this is hardcoded for our data 
+house_running_cost_main_grid = [0] * number_of_houses
+house_running_cost_micro_grid = [0] * number_of_houses
+
+#load csv data into pandas dataframes
+print("loading data for household energy usage")
+house1_df = pandas.read_csv(os.path.join(os.getcwd(),"year1.txt"))
+house2_df = pandas.read_csv(os.path.join(os.getcwd(),"year2.txt"))
+house3_df = pandas.read_csv(os.path.join(os.getcwd(),"year3.txt"))
+house4_df = pandas.read_csv(os.path.join(os.getcwd(),"year4.txt"))
+#collect dataframes into list and modify
+df_list = [house1_df, house2_df, house3_df, house4_df]
+x = 0
+for df in df_list:
+    #drop un-needed data 
+    df = df.drop(['Global_reactive_power', 'Voltage', 'Global_intensity', 'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'], axis=1)
+    #string manipulation, this can probably be done better but it's okay for now
+    for (columnName, columnData) in df.iteritems():
+        if columnName == "Date":
+            i = 0
+            dates_new = ['']*len(columnData)
+            for date in columnData:
+                dates_new 
+                date_list = date.split('/')
+                date_new = date_list[1] + '/' + date_list[0]
+                dates_new[i] = date_new
+                i+=1
+            print(dates_new)
+    df = df.drop(['Date'], axis=1)
+    df['Date'] = dates_new
+    df_list[x] = df
+    x+=1
+
+####################################################################
+# MAIN LOOP
+####################################################################
+print("Starting main loop")
 while interval_count != 0:
 
+    ##################################
     #decrement interval
-    print(interval_count)
+    #print(interval_count)
     interval_count -= 1
     
 
@@ -112,28 +147,33 @@ while interval_count != 0:
     #TODO add some sort of logic for this
     amount = 4
     battery.charge(amount, get_maingrid_cost()) #charge from main grid
-        
 
+    #TODO get demand for energy
+    house_demand = [0] * number_of_houses
+    for i_interval in range(interval_length): #iterate through interval length
+        #get date & time
+        date_ = str(dt.month) + '/' + str(dt.day)
+        time_ = str(dt.time()).split('.')[0]
+        print('date {} time {}'.format(date_,time_))
+        for i_num_houses in range(number_of_houses):
+            df_tmp = df_list[i_num_houses]
+            energy = float(df_tmp.loc[(df_tmp['Date']==date_) & (df_tmp['Time']==time_)]['Global_active_power'].item())/60
+            house_demand[i_num_houses] += energy 
+        dt += datetime.timedelta(minutes=1) #increment date time
+    
     #TODO discharge battery per 4 houses
-    for x in range(number_of_houses):
-        date_inq = dt.date()
-        time_inq = dt.time()
-        energy_used = house_data_call(date_inq, time_inq, interval_length, x)
-        if (battery.average_cost < get_maingrid_cost()) and (battery.current_charge > energy_used):
-            house_running_cost[x] += battery.discharge(energy_used)
+    for j_num_houses in range(number_of_houses):
+        #energy_used == house_demand
+        if (battery.average_cost < get_maingrid_cost()) and (battery.current_charge > house_demand[j_num_houses]):
+            house_running_cost_micro_grid[j_num_houses] += battery.discharge(house_demand[j_num_houses])
         else:
-            house_running_cost[x] += energy_used * get_maingrid_cost()
+            house_running_cost_main_grid[j_num_houses] += house_demand[j_num_houses] * get_maingrid_cost()
+    print("maingird: {}".format(house_running_cost_main_grid))
+    print("microgird: {}".format(house_running_cost_micro_grid))
 
-
-    #add increment_length  to current date time
-    dt += datetime.timedelta(minutes=interval_length)
-    print(dt)
-
-
-
-
-
-##### END
+##################################
+# END
 timer_end = time.time()
 print('finished in {} seconds'.format(timer_end-timer_start))
-
+for x in range(number_of_houses):
+    print('house {} paid {} dollars for their energy consumed in {} minutes'.format(x, house_running_cost[x], interval_count_historic * interval_length))
