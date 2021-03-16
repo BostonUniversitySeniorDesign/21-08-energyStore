4e060b01ce49ec55041901b20117c27922aebc25
psiozios <53226901+psiozios@users.noreply.github.com>
2021-03-16 16:05:34 -0400

    Update learning_model.py

--- ML/learning_model.py
+++ ML/learning_model.py
@@ -1,15 +1,33 @@
-##### IMPORTS ########
+#1 Importing libraries
 import numpy as np
-import sklearn
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import pandas as pd
 
+#2 Importing the dataset
+dataset = pd.read_csv('')
+X = dataset.iloc[:,1:2].values.astype(float)
+Y = dataset.iloc[:,2:3].values.astype(float)
+
+#3 Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_Y = StandardScaler()
+X = sc_X.fit_transform(X)
+Y = sc_Y.fit_transform(Y)
+
+#4 Fitting the Support Vector Regression Model to the dataset
+    #Creating support vector regressor here
 from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X,Y)
 
+#5 Predicting a new result
+Y_pred = regressor.predict('SOMETHING')
 
-##### Dataset ####### 
-    #TODO: get data from updated simulation 
-
-##### Feature Scaling / Normalization ##### 
-    #TODO: 
-
-
+#6 Visualising the Support Vector Regression results
+plt.scatter(X, Y, color = 'magenta')
+plt.plot(X, regressor.predict(X), color = 'green')
+plt.title('MicroGrid Energy Support Vector Regression Model')
+plt.xlabel('SOMETHING')
+plt.ylabel('SOMETHING')
+plt.show()
